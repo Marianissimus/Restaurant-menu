e19a898444062683afb92d14548e3f0328d03e64
Marianissimus <marianissimus@gmail.com>
2023-06-28 21:51:53 +0300

    display the menu items

--- restaurant-menu/src/components/Menu.vue
+++ restaurant-menu/src/components/Menu.vue
@@ -2,23 +2,28 @@
   <div class="menu">
     <h1>{{ msg }}</h1>
     <ul>
-      <li v-for="item in menuItems" :key="item.id">
-        <MenuItemPreview :item="item" />
+      <li v-for="item in items" :key="item.id">
+        <MenuItemPreview :currentItem="item" />
       </li>
     </ul>
   </div>
 </template>
 
-<script setup>
+<script>
 import menu from '@/data/menu.json'
 import MenuItemPreview from '@/components/MenuItemPreview'
-const items = menu.menu
-
 import { ref } from "vue"
-// import {ref, onMounted, watch} from "vue";
 
-const menuItems = ref(items);
-// items.value.push(...menuItems)
-// // const text = ref("");
-
+export default {
+  components: {
+    MenuItemPreview
+  },
+  setup() {
+    const items = menu.menu
+    const menuItems = ref(items);
+    return {
+      items: menuItems
+    }
+  }
+}
 </script>
--- restaurant-menu/src/components/MenuItemPreview.vue
+++ restaurant-menu/src/components/MenuItemPreview.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-
-    {{ item }}
-<!--     <div class="description">{{ item.description }}</div>
+    <div>{{ item }}</div>
+    <div class="name">{{ item.name }}</div>
+    <div class="description">{{ item.description }}</div>
+    <div class="image">
+      <img :src="require(`@/assets/menu/${item.image}_sm.jpg`)"
+      />
+    </div>
     <div class="price">{{ item.price }}</div>
-    <div class="rating">{{ item.rating }}</div> -->
+    <div class="rating">{{ item.rating }}</div>
+    <div class="type">{{ item.type }}</div>
   </div>
 </template>
 
-<script setup>
-import { defineProps } from 'vue';
-const item = defineProps(['item'])
+<script>
+import { toRefs } from "vue"
+
+export default {
+  props: ['currentItem'],
+  setup(props) {
+    const item = toRefs(props)
+    return {
+      item: item.currentItem
+    }
+  }
+}
 
 </script>
