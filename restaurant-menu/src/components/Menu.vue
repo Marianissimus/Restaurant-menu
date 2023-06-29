69f674c20b0e634fde26d51557cc8b6be94abf98
Marianissimus <marianissimus@gmail.com>
2023-06-29 13:13:42 +0300

    small shopping cart design

--- restaurant-menu/src/components/Menu.vue
+++ restaurant-menu/src/components/Menu.vue
@@ -1,4 +1,5 @@
 <template>
+  <ShoppingCart />
   <div class="menu">
     <ul class="menu-list">
       <li v-for="item in items" :key="item.id">
@@ -11,11 +12,13 @@
 <script>
 import menu from '@/data/menu.json'
 import MenuItemPreview from '@/components/MenuItemPreview'
+import ShoppingCart from '@/components/ShoppingCart'
 import { ref } from "vue"
 
 export default {
   components: {
-    MenuItemPreview
+    MenuItemPreview,
+    ShoppingCart,
   },
   setup() {
     const items = menu.menu
---
+++ restaurant-menu/src/components/ShoppingCart.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="shopping-cart">
+    Your cart
+    <img src="@/assets/shoppingCart.svg" class="shopping-cart-img">
+    <div class="how-many-items">3</div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    console.log('in here')
+  }
+}
+</script>
+
+<style scoped>
+.shopping-cart {
+  position: fixed;
+  top: .4em;
+  right: .4em;
+  width: 100px;
+  height: 100px;
+  background-color: white;
+  padding-top: 10px;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+  cursor: pointer;
+}
+
+.shopping-cart-img {
+  width: 50px;
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.how-many-items {
+  position: absolute;
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #A0334B;
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
